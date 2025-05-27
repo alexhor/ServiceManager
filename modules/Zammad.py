9b8f947af293ea7e5df175c4974d1f64957edcc5
Alexander Dennenmoser <alexander.dennenmoser@outlook.de>
2025-05-27 13:55:23 +0000

    Fixed Zammad module

--- modules/Zammad.py
+++ modules/Zammad.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 
+from os import chown
+from os.path import join
+
 from .Webserver import Webserver
 from .Module import Module
 
 
-class Zammad(Webserver, Module):
+class Zammad(Module):
     def __init__(self, subDomain):
         """A Zammad installation
         
         Args:
             subDomain (SubDomain): The subdomain this module is installed on
         """
-        self.requiredDirs = ['zammad', 'zammad-backup', 'elasticsearch', 'postgresql']
+        self.requiredDirs = ['zammad', 'zammad-backup', 'elasticsearch', 'postgresql', 'redis']
         super().__init__(subDomain)
+        # Fix elasticsearch permissions
+        chown(join(self.subDomain.rootDir, 'elasticsearch'), 1001, 1001)
 
     def _createEnvFile(self):
         """Put all requried parameters into an .env file in the subdomains root directory"""
@@ -22,7 +27,7 @@ class Zammad(Webserver, Module):
             envFile.write('DOMAIN_ESCAPED=' + str(self.subDomain).replace('.', '-') + '\n')
             envFile.write('HTTP_PORT=' + str(self.exposedPort) + '\n')
             envFile.write('DOMAIN_PATH=' + self.subDomain.rootDir + '\n')
-            envFile.write('POSTGRES_USER=zammad\n')
+            envFile.write('POSTGRES_USER=afaf\n')
             envFile.write('POSTGRES_PASSWORD=' + self.password(30) + '\n')
             envFile.write('IMAGE_REPO=zammad/zammad-docker-compose\n')
             envFile.write('VERSION=-latest\n')
