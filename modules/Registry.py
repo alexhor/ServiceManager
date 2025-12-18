75db0a7e9c83c1b1233e2bcdb43e37511081846b
Jonathan Straub <j.f.straub@gmx.de>
2025-12-18 13:27:46 +0100

    Registry: diverse fixes

--- modules/Registry.py
+++ modules/Registry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import os
+import shutil
 import subprocess
 from os.path import join, exists
+from pathlib import Path
 
 from .Module import Module
 
@@ -15,18 +17,24 @@ class Registry(Module):
     envKeyPass = 'REGISTRY_BUILDER_PASS'
 
     def __init__(self, subDomain):
-        self.htpasswdFile = join(self.subDomain.rootDir, 'htpasswd')
-
-        subprocess.call(['git', 'clone', 'https://github.com/JonFStr/ContainerBuilder', 'builder'])
-
         self.requiredDirs = ['builder', 'registry']
         super().__init__(subDomain)
 
+        self.htpasswdFile = join(self.subDomain.rootDir, 'htpasswd')
+
+        if not exists(join(self.subDomain.rootDir, 'builder')):
+            subprocess.call(['git', 'clone', 'https://github.com/JonFStr/ContainerBuilder', 'builder'],
+                            cwd=self.subDomain.rootDir)
+            # Copy default crontab
+            shutil.copy(join(self.subDomain.rootDir, 'builder', 'crontab.dist'),
+                        join(self.subDomain.rootDir, 'builder', 'crontab'))
+
         # Save passwords to htpasswd
         if not exists(self.htpasswdFile):
+            Path(self.htpasswdFile).touch()
             subprocess.call(
                 ['htpasswd', '-Bb', self.htpasswdFile, self.envVars[self.envKeyUser], self.envVars[self.envKeyPass]])
-            registryPwd = self.password(256)
+            registryPwd = self.password(255)
             subprocess.call(['htpasswd', '-Bb', self.htpasswdFile, 'registry', registryPwd])
             print('The following users were saved to $DOMAIN_PATH/htpasswd:')
             print(f'Username: {self.envVars[self.envKeyUser]} --- Password: {self.envVars[self.envKeyPass]}')
@@ -36,7 +44,7 @@ class Registry(Module):
     def _getCustomEnvVars(self) -> dict[str, str]:
         return {
             self.envKeyUser: 'builder',
-            self.envKeyPass: self.password(256),
+            self.envKeyPass: self.password(255),
         }
 
     def clean(self):
